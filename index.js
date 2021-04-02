329f04e58705f5c730c351e514e0490c96b7f9ad
karinesouza <venerandokarine@gmail.com>
2021-04-02 19:49:25 -0300

    resolvendo bugs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-console */
+/* eslint-disable semi */
+/* eslint-disable import/newline-after-import */
 const express = require('express')
 const routes = require('./server/routes/index')
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
